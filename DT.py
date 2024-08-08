46420ba159d6c009370cdb2f84e19376571f1d42
Zumzu <74483517+Zumzu@users.noreply.github.com>
2024-08-08 13:43:38 -0600

    Work on DT, implemented clickable text boxes

--- DT.py
+++ DT.py
@@ -1,41 +1,45 @@
+import pygame_textinput
 import pygame as game
 from sys import exit
 
-#ZUMZU.DEV DOWN
+from Modules.Base import Unit
+from Modules.Generator import findGun,findArmour
 
-
+WIDTH=1429
+HEIGHT=789
 
 game.init() 
 
 background=game.image.load('DT/Hexagons.png')
-WIDTH,HEIGHT=background.get_rect().size
+background=game.transform.scale(background,(WIDTH,HEIGHT))
 screen = game.display.set_mode((WIDTH,HEIGHT)) 
 background=background.convert_alpha()
-
 game.display.set_caption("Damage Tracker Mk2")
 game.display.set_icon(game.image.load('DT/EngineIco.png'))
-font=game.font.Font(None,50)
+monospacedLarge=game.font.SysFont('consolas',30)
+monospaced=game.font.SysFont('consolas',20)
   
-def updateArrows(event):
-    if event.type == game.KEYDOWN:
-        if event.key == game.K_LEFT:
-            pressedArrows[0]=True
-        elif event.key == game.K_RIGHT:
-            pressedArrows[1]=True
-        elif event.key == game.K_UP:
-            pressedArrows[2]=True
-        elif event.key == game.K_DOWN:
-            pressedArrows[3]=True
+def updateArrows(events):
+    for event in events:
+        if event.type == game.KEYDOWN:
+            if event.key == game.K_LEFT:
+                pressedArrows[0]=True
+            elif event.key == game.K_RIGHT:
+                pressedArrows[1]=True
+            elif event.key == game.K_UP:
+                pressedArrows[2]=True
+            elif event.key == game.K_DOWN:
+                pressedArrows[3]=True
 
-    if event.type == game.KEYUP:
-        if event.key == game.K_LEFT:
-            pressedArrows[0]=False
-        elif event.key == game.K_RIGHT:
-            pressedArrows[1]=False
-        elif event.key == game.K_UP:
-            pressedArrows[2]=False
-        elif event.key == game.K_DOWN:
-            pressedArrows[3]=False
+        if event.type == game.KEYUP:
+            if event.key == game.K_LEFT:
+                pressedArrows[0]=False
+            elif event.key == game.K_RIGHT:
+                pressedArrows[1]=False
+            elif event.key == game.K_UP:
+                pressedArrows[2]=False
+            elif event.key == game.K_DOWN:
+                pressedArrows[3]=False
 
 def fill(surface,rgb):
     w,h = surface.get_size()
@@ -44,22 +48,13 @@ def fill(surface,rgb):
         for j in range(h):
             surface.set_at((i,j),game.Color(r,g,b,surface.get_at((i,j))[3]))
 
-headImg=game.image.load('DT/Body/Head.png').convert_alpha()
-torsoImg=game.image.load('DT/Body/Torso.png').convert_alpha()
-larmImg=game.image.load('DT/Body/Larm.png').convert_alpha()
-rarmImg=game.image.load('DT/Body/Rarm.png').convert_alpha()
-llegImg=game.image.load('DT/Body/Lleg.png').convert_alpha()
-rlegImg=game.image.load('DT/Body/Rleg.png').convert_alpha()
-
-tempX=WIDTH//2
-tempY=HEIGHT//2
-pressedArrows=[False,False,False,False]
-
-tempText=font.render("Hello",True,(255,255,255))
-
 def frame(x:int,y:int,dx:int,dy:int):
     game.draw.rect(screen, (30,30,30), game.Rect(x,y,dx,dy), border_radius=5)
-    game.draw.rect(screen, (60,60,60), game.Rect(x+3,y+3,dx-6,dy-6), border_radius=1)
+    game.draw.rect(screen, (180,180,180), game.Rect(x+3,y+3,dx-6,dy-6), border_radius=1)
+
+def buttonFrame(x:int,y:int,dx:int,dy:int):
+    game.draw.rect(screen, (30,30,30), game.Rect(x,y,dx,dy), border_radius=5)
+    game.draw.rect(screen, (220,220,220), game.Rect(x+3,y+3,dx-6,dy-6), border_radius=1)
 
 def drawDude(x:int,y:int):
     screen.blit(headImg,(x+76,y+1))
@@ -68,16 +63,58 @@ def drawDude(x:int,y:int):
     screen.blit(rarmImg,(x+131,y+76))
     screen.blit(llegImg,(x+23,y+215))
     screen.blit(rlegImg,(x+94,y+213))
+    
+    #drawPointer(x+170,y+150)
+    #drawPointer(x+135,y+327)
+    #drawPointer(x+31,y+151,True)
+    #drawPointer(x+55,y+321,True)
+
+def drawPointer(x:int,y:int,flip:bool=False):
+    if flip:
+        game.draw.line(screen, (0,0,0), (x,y), (x-30,y-30), 2)
+        game.draw.line(screen, (0,0,0), (x-30,y-30), (x-100,y-30), 2)
+    else:
+        game.draw.line(screen, (0,0,0), (x,y), (x+30,y-30), 2)
+        game.draw.line(screen, (0,0,0), (x+30,y-30), (x+100,y-30), 2)
+
+headImg=game.image.load('DT/Body/Head.png').convert_alpha()
+torsoImg=game.image.load('DT/Body/Torso.png').convert_alpha()
+larmImg=game.image.load('DT/Body/Larm.png').convert_alpha()
+rarmImg=game.image.load('DT/Body/Rarm.png').convert_alpha()
+llegImg=game.image.load('DT/Body/Lleg.png').convert_alpha()
+rlegImg=game.image.load('DT/Body/Rleg.png').convert_alpha()
+
+tempX=WIDTH//4
+tempY=HEIGHT//4
+pressedArrows=[False,False,False,False]
+
+damageTrack1=monospaced.render("[#####][#####][###..][.....][.....]",True,(0,0,0))
+damageTrack2=monospaced.render("[.....][.....][.....][.....][.....]",True,(0,0,0))
+
+loadTextLabel=monospacedLarge.render("Load",True,(0,0,0))
+loadInput=pygame_textinput.TextInputVisualizer()
+loadInput.font_object=monospaced
+loadSelected=False
+loadHitbox=game.Rect(550,42,250,36)
 
 while True: 
-    for event in game.event.get(): 
+    events=game.event.get()
+    for event in events: 
         if event.type == game.QUIT: 
             print(round(tempX),"|",round(tempY))
             game.quit()
             exit()
-        
-        updateArrows(event)
+
+        if event.type == game.MOUSEBUTTONUP:
+            loadSelected=loadHitbox.collidepoint(game.mouse.get_pos())
+
+    if loadSelected:
+        loadInput.update(events)
+    else:
+        loadInput.cursor_visible=False
     
+        
+    updateArrows(events)
     if pressedArrows[0]:
         tempX-=0.1
     if pressedArrows[1]:
@@ -88,9 +125,14 @@ while True:
         tempY+=0.1
     
     screen.blit(background,(0,0))
-    frame(30,30,WIDTH-60,500)
-    drawDude(52,43)
-    
-    #screen.blit(tempText,(tempX,tempY))
+    frame(30,30,400,600)
+    frame(460,30,WIDTH-490,600)
+    drawDude(133,63)
+
+    screen.blit(damageTrack1,(38,575))
+    screen.blit(damageTrack2,(38,600))
+    screen.blit(loadTextLabel,(473,45))
+    buttonFrame(550,42,250,36)
+    screen.blit(loadInput.surface,(556,50))
 
     game.display.update() 
