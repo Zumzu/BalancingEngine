083fd68c140e92f19a7d82d4dc8eb22b1b63f7c2
Zumzu <74483517+Zumzu@users.noreply.github.com>
2024-08-09 19:20:13 -0600

    Wound track and wound hitboxes

--- DT.py
+++ DT.py
@@ -8,6 +8,8 @@ from Modules.Generator import findGun,findArmour
 WIDTH=1429 #actively clean multiples of hexagons native resolution
 HEIGHT=789
 
+WOUNDCOLOR=(169,18,1)
+
 game.init() 
 
 background=game.image.load('DT/Hexagons.png')
@@ -18,6 +20,7 @@ game.display.set_caption("Damage Tracker Mk2")
 game.display.set_icon(game.image.load('DT/EngineIco.png'))
 monospacedLarge=game.font.SysFont('consolas',30)
 monospaced=game.font.SysFont('consolas',20)
+monospacedSmall=game.font.SysFont('consolas',15)
   
 def updateArrows(events):
     for event in events:
@@ -52,9 +55,16 @@ def frame(x:int,y:int,dx:int,dy:int):
     game.draw.rect(screen, (30,30,30), game.Rect(x,y,dx,dy), border_radius=5)
     game.draw.rect(screen, (180,180,180), game.Rect(x+3,y+3,dx-6,dy-6), border_radius=1)
 
-def buttonFrame(x:int,y:int,dx:int,dy:int):
+def inputFrame(x:int,y:int,dx:int,dy:int):
     game.draw.rect(screen, (30,30,30), game.Rect(x,y,dx,dy), border_radius=5)
     game.draw.rect(screen, (220,220,220), game.Rect(x+3,y+3,dx-6,dy-6), border_radius=1)
+
+def buttonFrame(x:int,y:int,dx:int,dy:int,hover:bool):
+    game.draw.rect(screen, (30,30,30), game.Rect(x,y,dx,dy), border_radius=5)
+    if hover:
+        game.draw.rect(screen, (150,150,150), game.Rect(x+3,y+3,dx-6,dy-6), border_radius=1)
+    else:
+        game.draw.rect(screen, (80,80,80), game.Rect(x+3,y+3,dx-6,dy-6), border_radius=1)
 
 def drawDude(x:int,y:int):
     screen.blit(headImg,(x+76,y+1))
@@ -64,10 +74,10 @@ def drawDude(x:int,y:int):
     screen.blit(llegImg,(x+23,y+215))
     screen.blit(rlegImg,(x+94,y+213))
     
-    #drawPointer(x+170,y+150)
-    #drawPointer(x+135,y+327)
-    #drawPointer(x+31,y+151,True)
-    #drawPointer(x+55,y+321,True)
+    drawPointer(x+170,y+150)
+    drawPointer(x+135,y+327)
+    drawPointer(x+31,y+151,True)
+    drawPointer(x+55,y+321,True)
 
 def drawPointer(x:int,y:int,flip:bool=False):
     if flip:
@@ -88,20 +98,100 @@ tempX=0
 tempY=0
 pressedArrows=[False,False,False,False]
 
-damageTrack1=monospaced.render("[#####][#####][###..][.....][.....]",True,(0,0,0))
-damageTrack2=monospaced.render("[.....][.....][.....][.....][.....]",True,(0,0,0))
-
 loadTextLabel=monospacedLarge.render("Load",True,(0,0,0))
 loadInput=pygame_textinput.TextInputVisualizer()
 loadInput.font_object=monospaced
+loadInput.manager.validator=(lambda x: len(x)<=21 and str(x).isprintable())
 loadSelected=False
-loadHitbox=game.Rect(550,42,250,36)
+loadHitbox=game.Rect(550,42,246,36)
+def loadBlit():
+    screen.blit(loadTextLabel,(473,45))
+    inputFrame(550,42,246,36)
+    screen.blit(loadInput.surface,(556,50))
 
 damageTextLabel=monospacedLarge.render("DAMAGE",True,(0,0,0))
 damageInput=pygame_textinput.TextInputVisualizer()
 damageInput.font_object=monospacedLarge
+damageInput.manager.validator=(lambda x: x=='' or (len(x)<=6 and (str(x[-1]).isnumeric() or str(x[-1]).lower()=='d') or x[-1]=='+' or x[-1]=='-'))
 damageSelected=False
-damageHitbox=game.Rect(506,320,100,46)
+damageHitbox=game.Rect(506,533,116,46)    
+def damageBlit():
+    screen.blit(damageTextLabel,(508,503))
+    inputFrame(506,533,116,46)
+    screen.blit(damageInput.surface,(512,542))
+
+multiplierTextLabel=monospacedLarge.render("X",True,(50,50,50))
+multiplierEmptyFieldLabel=monospacedLarge.render("1",True,(50,50,50))
+multiplierInput=pygame_textinput.TextInputVisualizer()
+multiplierInput.font_object=monospacedLarge
+multiplierInput.manager.validator=(lambda x: len(x)<=2 and (str(x).isnumeric() or x==''))
+multiplierSelected=False
+multiplierHitbox=game.Rect(656,533,40,46)
+def multiplierBlit():
+    inputFrame(656,533,47,46)
+    screen.blit(multiplierTextLabel,(631,543))
+    screen.blit(multiplierInput.surface,(663,542))
+    if multiplierInput.value=='':
+        screen.blit(multiplierEmptyFieldLabel,(663,542))
+
+pewTextLabel=monospaced.render("PEW!",True,(0,0,0))
+pewPewTextLabel=monospaced.render("PEW PEW!",True,(0,0,0))
+pewHitbox=game.Rect(724,538,100,36)
+def pewBlit():
+    buttonFrame(724,538,110,36,pewHitbox.collidepoint(game.mouse.get_pos()))
+    if multiplierInput.value=='' or multiplierInput.value=='1':
+        screen.blit(pewTextLabel,(759,547))
+    else:
+        screen.blit(pewPewTextLabel,(735,547))
+
+
+woundTrackText=['LIGHT','SERIOUS','CRITICAL']
+for i in range(7):
+    woundTrackText.append(f"MORTAL_{i}")
+
+woundTrackLabels=[]
+for text in woundTrackText:
+    woundTrackLabels.append(monospacedSmall.render(text,True,(0,0,0)))
+
+def drawWoundTrack(startX:int,startY:int,endX:int,buffer:int,wounds:int):
+    wounds=max(0,min(50,wounds))
+    boxSize=(endX-startX-buffer*10)/50
+    offset=0
+    for i in range(10):
+        offset+=boxSize/2
+        if i==5:
+            offset+=boxSize/2
+        rect=woundTrackLabels[i].get_rect(center=(startX+(i+0.5)*5*boxSize+offset,startY-9))
+        screen.blit(woundTrackLabels[i],rect)
+        drawWoundSet(startX+i*5*boxSize+offset,startY,boxSize,wounds-5*i)
+
+def drawWoundSet(startX:int,startY:int,boxSize:float,wounds:int):
+    game.draw.line(screen,(0,0,0),(startX,startY),(startX+boxSize*5,startY),2)
+    game.draw.line(screen,(0,0,0),(startX,startY+boxSize),(startX+boxSize*5,startY+boxSize),2)
+    for i in range(6):
+        game.draw.line(screen,(0,0,0),(startX+boxSize*i,startY),(startX+boxSize*i,startY+boxSize),2)
+    for i in range(max(0,min(wounds,5))):
+        game.draw.line(screen,WOUNDCOLOR,(startX+boxSize*i+6,startY+3),(startX+boxSize*i+int(boxSize)-5,startY+boxSize-2),8)
+        game.draw.line(screen,WOUNDCOLOR,(startX+boxSize*i+6,startY+int(boxSize)-2),(startX+boxSize*i+int(boxSize)-5,startY+3),8)
+
+def drawWounds(wounds):
+    frame(30,645,WIDTH-60,HEIGHT-675)
+    drawWoundTrack(60,690,WIDTH-160,4,wounds)
+
+def generateWoundHitboxes(startX:int,startY:int,endX:int,buffer:int):
+    output=[]
+    boxSize=(endX-startX-buffer*10)/50
+    offset=0
+    for i in range(10):
+        offset+=boxSize/2
+        if i==5:
+            offset+=boxSize/2
+        for j in range(5):
+            output.append(game.Rect((startX+i*5*boxSize+offset)+boxSize*j,startY,boxSize,boxSize))
+
+    return output
+
+woundTrackHitBoxes=generateWoundHitboxes(60,690,WIDTH-160,4)
 
 while True: 
     events=game.event.get()
@@ -114,10 +204,19 @@ while True:
         if event.type == game.MOUSEBUTTONUP:
             loadSelected=loadHitbox.collidepoint(game.mouse.get_pos())
             damageSelected=damageHitbox.collidepoint(game.mouse.get_pos())
+            multiplierSelected=multiplierHitbox.collidepoint(game.mouse.get_pos())
 
         if event.type == game.KEYDOWN and event.key == game.K_RETURN:
+            if damageSelected or multiplierSelected:
+                print(damageInput.value,'x',multiplierInput.value)
+
+        if event.type == game.KEYDOWN and event.key == game.K_TAB:
             if damageSelected:
-                print(damageInput.value)
+                damageSelected=False
+                multiplierSelected=True
+            elif multiplierSelected:
+                multiplierSelected=False
+                damageSelected=True
 
 
     if loadSelected:
@@ -127,34 +226,37 @@ while True:
 
     if damageSelected:
         damageInput.update(events)
+        if damageInput.value!='' and damageInput.value[-1]=='d':
+            damageInput.value=damageInput.value[:-1]+'D'
     else:
         damageInput.cursor_visible=False
+
+    if multiplierSelected:
+        multiplierInput.update(events)
+    else:
+        multiplierInput.cursor_visible=False
     
         
     updateArrows(events)
     if pressedArrows[0]:
-        tempX-=0.1
+        tempX-=0.2
     if pressedArrows[1]:
-        tempX+=0.1
+        tempX+=0.2
     if pressedArrows[2]:
-        tempY-=0.1
+        tempY-=0.2
     if pressedArrows[3]:
-        tempY+=0.1
+        tempY+=0.2
     
     screen.blit(background,(0,0))
     frame(30,30,400,600)
     frame(460,30,WIDTH-490,600)
     drawDude(133,63)
 
-    screen.blit(damageTrack1,(38,575))
-    screen.blit(damageTrack2,(38,600))
+    loadBlit()
+    damageBlit()
+    multiplierBlit()
+    pewBlit()
 
-    screen.blit(loadTextLabel,(473,45))
-    buttonFrame(550,42,250,36)
-    screen.blit(loadInput.surface,(556,50))
-
-    screen.blit(damageTextLabel,(506,290))
-    buttonFrame(506,320,100,46)
-    screen.blit(damageInput.surface,(512,328))
+    drawWounds(24)
 
     game.display.update() 
