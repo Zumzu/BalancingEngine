170fbe6fce30d4c37d55ba6234f5c82afdbcff10
Zumzu <74483517+Zumzu@users.noreply.github.com>
2024-10-17 01:02:25 -0600

    Funny skill counter and autofill skill field done

--- TheCodex.py
+++ TheCodex.py
@@ -1,7 +1,8 @@
 from PIL import ImageTk, Image
 from tkinter import *
-from tkinter.ttk import Entry,Combobox
+from tkinter.ttk import Entry
 from tkinterdnd2 import DND_FILES, TkinterDnD
+from ttkwidgets.autocomplete import AutocompleteCombobox
 
 from Modules.Character import Character
 
@@ -39,16 +40,16 @@ consolasSmall = ("Consolas", 16)
 
 def addSkillPrompt():
     suggestions = [skillName for stat in char.skillSet.skillList for skillName, skillValue in stat.get('skills', {}).items() if skillValue == 0]
+    suggestions.extend(['surgery','solo','fixer','tactician','tech','ronin','netrunner','medtech'])
 
     newSkillWindow = Toplevel()
     newSkillWindow.title("Add New Skill")
     Label(newSkillWindow, text="Skill Name:", font=consolasSmall).grid(row=0, column=0, sticky="w", padx=5, pady=5)
     
-    skillEntry = Combobox(newSkillWindow, values=suggestions, font=consolasSmall, width=20)
+    skillEntry = AutocompleteCombobox(newSkillWindow, completevalues=suggestions, font=consolasSmall, width=30)
     skillEntry.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
     skillEntry.set("")
-    skillEntry.after(100, skillEntry.focus)
-    skillEntry.after(100, skillEntry.event_generate, "<Down>")
+    skillEntry.after(100, skillEntry.focus_set())
 
     def addSkill():
         skillName = skillEntry.get().strip()
@@ -57,13 +58,15 @@ def addSkillPrompt():
             refreshSkills()
             newSkillWindow.destroy()
 
+    skillEntry.bind('<Return>',lambda _: addSkill())
     addButton = Button(newSkillWindow, text="Add Skill", command=addSkill, font=consolasSmall)
     addButton.grid(row=2, columnspan=2, pady=10)
 
 addSkillButton = Button(skillsFrame, text="+", command=addSkillPrompt, font=consolasSmall)
-addSkillButton.grid(row=0, pady=5, sticky="w")
+addSkillButton.grid(row=0,column=1, padx=5, sticky="ew")
 def refreshSkills():
-    for widget in skillsFrame.winfo_children()[1:]:
+    updateSkillSum()
+    for widget in skillsFrame.winfo_children()[2:]:
         widget.destroy()
 
     row = 1
@@ -102,19 +105,20 @@ def refreshSkills():
                 row += 1
 
 def populateStatsFrame():
+    updateStatSum()
     for index, stat in enumerate(char.skillSet.skillList[:-1]):
-        for i in range(len(char.skillSet.skillList) - 1):
+        for i in range(len(char.skillSet.skillList)):
             statsFrame.rowconfigure(i, weight=1)
 
         label = Label(statsFrame, text=str(stat['name']).capitalize(), anchor="w", font=consolas)
-        label.grid(row=index, column=0, sticky="w")
+        label.grid(row=index+1, column=0, sticky="w")
 
         valueEntry = Entry(statsFrame, font=consolas, width=3, justify="center")
         valueEntry.insert(0, str(stat['value']))
-        valueEntry.grid(row=index, column=1, sticky="ew", padx=3)
+        valueEntry.grid(row=index+1, column=1, sticky="ew", padx=3)
 
         warningLabel = Label(statsFrame, text="⚠️", font=consolas, fg="red")
-        warningLabel.grid(row=index, column=2, sticky="w", padx=3)
+        warningLabel.grid(row=index+1, column=2, sticky="w", padx=3)
         warningLabel.grid_remove()
 
         valueEntry.bind("<KeyRelease>", lambda _, statName=stat['name'], entry=valueEntry, warning=warningLabel: statChanged(statName, entry, warning))
@@ -136,11 +140,43 @@ def populateStatsFrame():
         warningLabel.bind("<Enter>", showTooltip)
         warningLabel.bind("<Leave>", hideTooltip)
 
+statSumLabel=Label(statsFrame, text='?/60', anchor="w", font=consolasSmall)
+statSumLabel.grid(row=0, column=0, columnspan=3, sticky="ew")
+def updateStatSum():
+    statSum=0
+    questionMark=False
+    for stat in char.skillSet.skillList[:-1]:
+        value=int(stat['value'])
+        if value<3 or value>10:
+            questionMark=True
+        statSum+=value+max(0,value-8)
+
+    statSumLabel.config(text=f'{statSum}{"?" if questionMark else ""}/60')
+
+skillSumLabel=Label(skillsFrame, text='?/? | ?/?', anchor="w", font=consolasSmall)
+skillSumLabel.grid(row=0, column=0, sticky="ew")
+def updateSkillSum():
+    littleSkillSum=0
+    bigSkillSum=0
+    
+    for stat in char.skillSet.skillList:
+        for _,skillValue in stat.get('skills', {}).items():
+            if skillValue <= 3:
+                littleSkillSum += skillValue
+                if littleSkillSum > 24:
+                    bigSkillSum += littleSkillSum-24
+                    littleSkillSum = 24
+            else:
+                bigSkillSum += skillValue
+
+    skillSumLabel.config(text=f'{bigSkillSum}/{50+char.skillSet.getStat("int")*2} | {littleSkillSum}/24')
+
 def statChanged(statName: str, entry:Entry, warningLabel: Label):
     newValue=str(entry.get())
     if newValue.isnumeric() and 0 <= int(newValue) <= 20:
         char.skillSet.setStat(statName, int(newValue))
         refreshSkills()
+        updateStatSum()
         warningLabel.grid_remove()
     else:
         warningLabel.grid()
@@ -163,6 +199,7 @@ def skillChanged(skillName: str, entry:Entry, warningLabel: Label):
     newValue=str(entry.get())
     if newValue.isnumeric() and 0 <= int(newValue) <= 20:
         char.skillSet.setSkill(skillName, int(newValue))
+        updateSkillSum()
         warningLabel.grid_remove()
         if int(newValue)==0:
             refreshSkills()
@@ -176,7 +213,7 @@ def skillScrolled(event, skillName:str, entry:Entry, warningLabel: Label):
             entry.delete(0,"end")
             entry.insert(0, str(currentVal + 1))
             skillChanged(skillName,entry,warningLabel)
-        elif event.delta < 0 and currentVal > 3:
+        elif event.delta < 0 and currentVal > 1:
             entry.delete(0,"end")
             entry.insert(0, str(currentVal - 1))
             skillChanged(skillName,entry,warningLabel)
@@ -201,4 +238,5 @@ refreshSkills()
 window.protocol("WM_DELETE_WINDOW",onClose)
 window.drop_target_register(DND_FILES)
 window.dnd_bind('<<Drop>>', dropImage)
+window.iconphoto(False,PhotoImage(file='./DT_Images/Misc/EngineIco.png'))
 window.mainloop()
